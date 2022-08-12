d1845e09a2798cf938bdc51da45fa991724c7fff
Richard Kubíček <rici.kubicek@gmail.com>
2022-08-12 23:42:22 +0200

    Stage 1 completed

--- Simple Currency Converter/task/main.js
+++ Simple Currency Converter/task/main.js
@@ -1,1 +1,6 @@
-//Write your code here
+console.log('Welcome to Currency Converter!');
+console.log('1 USD equals 1 USD');
+console.log('1 USD equals 113.5 JPY');
+console.log('1 USD equals 0.89 EUR');
+console.log('1 USD equals 74.36 RUB');
+console.log('1 USD equals 0.75 GBP');
